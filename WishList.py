d287a746a0db17b0f626fa1818a618136e1c3996
Satyam Krishna <thesatyam10@gmail.com>
2021-08-19 14:29:13 +0530

    Update on AI

--- WishList.py
+++ WishList.py
@@ -1,30 +1,87 @@
 import datetime
+import random
+from plyer import notification
 import playsound
 from jarvis import speak
 
 
 def specialDay():
-    if datetime.date(2021, 6, 26) == datetime.date.today():
-        playsound.playsound("BirthdaySong.mp3")
+    if datetime.date(2022, 6, 26) == datetime.date.today():
+        wishList = ["Happy birthday sir", playsound.playsound("BirthdaySong.mp3")]
+        speak(random.Random(wishList))
         # webbrowser.open("https://music.youtube.com/watch?v=pk21s6tUjGY&list=RDAMVMpk21s6tUjGY")
 
+    elif datetime.date(2021, 8, 19) == datetime.date.today():
+        notification.notify(
+            title="Ankita's Birthday",
+            message="Wish them",
+            app_icon="E:\Jarvis AI\icon.ico",
+            timeout=5
+        )
+        # speak("Today is Ankita's birthday.")
+
     elif datetime.date(2021, 3, 29) == datetime.date.today():
+        notification.notify(
+            title="Today is Holy",
+            message="Happy Holy",
+            app_icon="E:\Jarvis AI\icon.ico",
+            timeout=5
+        )
         speak("Happy Holli sir.")
 
-    elif datetime.date(2021, 8, 15) == datetime.date.today():
+    elif datetime.date(2022, 8, 15) == datetime.date.today():
+        notification.notify(
+            title="Independence Day",
+            message="Happy Independence Day",
+            app_icon="E:\Jarvis AI\icon.ico",
+            timeout=5
+        )
         speak("Happy Independence Day sir")
 
     elif datetime.date(2021, 10, 15) == datetime.date.today():
+        notification.notify(
+            title="Dussehra",
+            message="Happy Dussehra sir",
+            app_icon="E:\Jarvis AI\icon.ico",
+            timeout=5
+        )
         speak("Happy Dussehra sir")
 
     elif datetime.date(2021, 11, 4) == datetime.date.today():
+        notification.notify(
+            title="Diwali",
+            message="Happy Diwali",
+            app_icon="E:\Jarvis AI\icon.ico",
+            timeout=5
+        )
         speak("Happy Diwali sir")
 
     elif datetime.date(2021, 11, 10) == datetime.date.today():
+        notification.notify(
+            title="Chhat Puja",
+            message="Happy Chhat puja",
+            app_icon="E:\Jarvis AI\icon.ico",
+            timeout=5
+        )
         speak("Happy Chhat puja sir.")
 
     elif datetime.date(2022, 1, 26) == datetime.date.today():
+        notification.notify(
+            title="Republic Day",
+            message="Happy Republic Day",
+            app_icon="E:\Jarvis AI\icon.ico",
+            timeout=5
+        )
         speak("Happy Republic day sir")
 
-    elif datetime.date(2021, 6, 6) == datetime.date.today():
-        speak("Hello Sir this date is same as today's date.")
+    elif datetime.date(2022, 4, 26) == datetime.date.today():
+        notification.notify(
+            title="Jarvis's Birthday",
+            message="Today is Birthday of your Jarvis's AI.",
+            app_icon="E:\Jarvis AI\icon.ico",
+            timeout=5
+        )
+        speak("Happy Birthday to me")
+
+    # elif datetime.date(2021, 6, 12) == datetime.date.today():
+    #     playsound.playsound("BirthdaySong.mp3")
